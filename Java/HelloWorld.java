b01fc71f955dd96064ca4709d418aa06da867e85
bill9108 <gzlinzhimin@gmail.com>
2017-04-18 18:08:13 -0700

    Demo framework

---
+++ Java/HelloWorld.java
@@ -0,0 +1,47 @@
+package tic_tac_toe;
+
+import java.util.Scanner;
+
+/**
+ *
+ */
+public class HelloWorld {
+    private static Scanner input;
+
+    private int magic (int x){
+        int output = 0;
+
+        //TODO: perform some arithmetic operation on x and pass the result to output
+
+        return output;
+    }
+
+    public static void main (String[] args){
+
+        //TODO: print "Hello World"
+
+        //TODO: Declare a few int variables (result) and initialize
+
+        //TODO: boolean variables and value comparison
+
+        //TODO: If-else
+
+        //TODO: create a string that is concatenated by two strings and print it out
+
+        //TODO: arrays
+
+        //TODO: write a for loop and equivalent while loop to initialize the array and print elements
+
+        //TODO: read a number from command line input and pass into a variable
+
+        //TODO: call magic() and store the return value in result
+
+        System.out.println(result);
+
+        //TODO: write another function?
+
+        //TODO: Generate a random number and store the number in result
+
+        System.out.println(result);
+    }
+}
