6341523f3b689474773eb6fc751e12c2ef104cce
bill9108 <gzlinzhimin@gmail.com>
2017-04-20 22:05:59 -0700

    Demo examples that will be written on class

--- Java/HelloWorld.java
+++ Java/HelloWorld.java
@@ -1,4 +1,4 @@
-package tic_tac_toe;
+
 
 import java.util.Scanner;
 
@@ -6,9 +6,8 @@ import java.util.Scanner;
  *
  */
 public class HelloWorld {
-    private static Scanner input;
 
-    private int magic (int x){
+    private static int magic (int x){
         int output = 0;
 
         //TODO: perform some arithmetic operation on x and pass the result to output
@@ -20,28 +19,85 @@ public class HelloWorld {
 
         //TODO: print "Hello World"
 
-        //TODO: Declare a few int variables (result) and initialize
+        //TODO: variable declaration
 
         //TODO: boolean variables and value comparison
 
         //TODO: If-else
 
         //TODO: create a string that is concatenated by two strings and print it out
+        /*String s0 = "Hello World!";
+        String s1 = s0 + "Welcome to Programing in the Sky!";
+        int x = 10;
+        String s2 = "x equals " + x;
+        String s3 = s1 + x;*/
 
         //TODO: arrays
+        //One dimentional array:
+        /*
+        int[] arr = new int[10];
+        int[] arr2 = {1, 2, 3, 4, 5};
+
+        //2D array:
+        int[][] arr2d = new int[3][3];
+        int[][] arr2d2 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+        //Set elements:
+        arr[0] = 100;
+        arr[1] = 101;
+        arr2d[0][0] = 0;  //Row 0, column 0
+        arr2d[0][1] = 1;  //Row 0, column 1
+
+        //Access elements:
+        int element1 = arr[0];
+        System.out.print(element1);
+
+        element 1 = arr2d[0][0];
+        System.out.print(element1);*/
 
         //TODO: write a for loop and equivalent while loop to initialize the array and print elements
+        /*int i = 0;
+        for (i = 0; i < 10; i++){
+            arr[i] = i;
+        }
+
+        //Equivalent while loop
+        while (i < 10){
+            arr[i] = i;
+            i++;
+        }
+
+        //Break
+        for (i = 0; i < 10; i++) {
+            if (i == 4) {
+                break;
+            }
+            arr2[i] = i;
+            i++;
+        }
+
+        //Continue
+        for (i = 0; i < 10; i++){
+            if (i == 8){
+                continue;
+            }
+            arr[i] = i * 10;
+        }*/
 
         //TODO: read a number from command line input and pass into a variable
+        /*Scanner input = new Scanner(System.in);
+        int inputNum = input.nextInt();
+        String inputStr = input.next();
+        char inputChar = input.next().charAt(0);*/
 
         //TODO: call magic() and store the return value in result
-
-        System.out.println(result);
+        //result = magic(10);
+        //System.out.println(result);
 
         //TODO: write another function?
 
         //TODO: Generate a random number and store the number in result
-
-        System.out.println(result);
+        /*result = (int) (Math.random() * 2);
+        System.out.println(result);*/
     }
 }
